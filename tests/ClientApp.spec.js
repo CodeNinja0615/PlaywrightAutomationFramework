94362b99fed9cf3956d98a3530fa4260272bcf27
CodeNinja0615 <sameerakhtar1513@gmail.com>
2025-01-27 00:24:19 +0530

    End-To-End Automation of Ecommerce website single product

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test('End-To-End test', async ({ page }) => {
+test('End-To-End Test', async ({ page }) => {
     // const productName = 'IPHONE 13 PRO';
     const email = 'akhtarsameer743@gmail.com';
     const productNames = ['IPHONE 13 PRO', 'Banarsi Saree', 'qwerty'];
@@ -70,4 +70,81 @@ test('End-To-End test', async ({ page }) => {
             expect(orderID).toContain(newOrderID);
         }
     }    
+});
+
+
+test.only('End-To-End Test 2', async ({ page }) => {
+    const productName = 'IPHONE 13 PRO';
+    const email = 'akhtarsameer743@gmail.com';
+    await page.goto('https://rahulshettyacademy.com/client/');
+    const products = page.locator('.card-body');
+    const title = await page.title();
+    await page.fill('#userEmail', email);
+    await page.fill('#userPassword', 'Sameerking01!');
+    await page.click('input#login');
+    await page.waitForLoadState('networkidle');
+    await page.locator('.card-body b').first().waitFor();
+    const titles = await page.locator('.card-body b').allTextContents();
+    console.log(titles);
+    const count = await products.count();
+    //iterate through the products
+    for (let i = 0; i < count; i++) {
+        // await page.pause();
+        const productToAdd = await products.nth(i).locator('b').textContent();
+        if(productName === productToAdd){
+            // await products.nth(i).locator('.btn.w-10.rounded').click();
+            await products.nth(i).locator('text=Add To Cart').click();
+            const toast = page.locator('.ng-trigger-flyInOut');
+            await toast.waitFor();
+            await toast.waitFor({ state: 'hidden' });
+            break;
+        }
+    }
+    // await page.pause();
+    await page.locator('[routerlink$="/dashboard/cart"]').click();
+
+    const cartProd = page.locator('.cartSection h3');
+    await cartProd.first().waitFor();
+    const cartCount = await cartProd.count();
+    for (let i = 0; i < cartCount; i++) {
+        const product = await cartProd.nth(i).textContent();
+        expect(productName).toEqual(product);
+    }
+
+    await page.locator('.totalRow .btn.btn-primary').click();
+    await page.locator('[placeholder="Select Country"]').pressSequentially ('India');
+    const dropdown = await page.locator('.ta-results');
+    await dropdown.waitFor();
+    const options = await dropdown.locator('button');
+    const optionsCount = await options.count();
+    for (let i = 0; i < optionsCount; i++) {
+        const text = await options.nth(i).textContent();
+        if (text === ' India') {
+            await options.nth(i).click();
+            break;
+        }
+    }
+    // await page.pause();
+    await expect(page.locator('.user__name label')).toHaveText(email);
+    await page.locator('.action__submit').click();
+    const success = page.locator('.hero-primary');
+    await expect(success).toHaveText('Thankyou for the order.');
+    const orderID = await page.locator('label.ng-star-inserted').textContent();
+    console.log(orderID);
+    await page.locator('button[routerlink*="/dashboard/myorders"]').click();
+    await page.locator('div h1:has-text("Your Orders")').waitFor();
+    const orderIDRow = page.locator('tbody tr');
+    for (let i = 0; i < await orderIDRow.count(); i++) {
+        const order = await orderIDRow.locator('th').nth(i).textContent();
+        console.log(order);
+        const text = order.split(" | ")[0];
+        console.log(text);
+        if(text.trim() === order){
+            await page.pause();
+            expect(text).toEqual(order);
+            await orderIDRow.first().locator('button.btn-primary').click();
+            await expect(page.locator('.address p.text:nth-child(2)').first()).toHaveText(email);
+            break;
+        }
+    }
 });
