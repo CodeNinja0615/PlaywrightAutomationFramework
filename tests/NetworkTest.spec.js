8ac61109d73e9a30a54715c3573d262f62b057a1
CodeNinja0615 <sameerakhtar1513@gmail.com>
2025-02-19 03:16:51 +0530

    Network intercept example

---
+++ tests/NetworkTest.spec.js
@@ -0,0 +1,46 @@
+const { test, expect, request } = require('@playwright/test');
+const {APIutils} = require('./utils/APIutils');
+
+const loginPayload = {userEmail: "akhtarsameer743@gmail.com", userPassword: "Sameerking01!"}; // Define the login payload
+const orderPayload = {orders:[{country:"Cuba",productOrderedId:"67a8df1ac0d3e6622a297ccb"}]};
+let response;
+const fakePayloadOrder = {data:[],message:"No Orders"}; //----Fake reponse body
+test.beforeAll(async () => {
+    const apiContext = await request.newContext(); // Create a new context for API requests
+    const apiUtils = new APIutils(apiContext, loginPayload);
+    response = await apiUtils.createOrder(orderPayload); //Don't forget await
+});
+
+// test.beforeEach(async ({ page }) => {
+// });
+
+test('Network Intercept', async ({ page }) => {
+    page.addInitScript(value =>{ // Add the token to the local storage
+        window.localStorage.setItem('token', value); // Set the token in the local storage
+    }, response.token); // Pass the token to the init script
+
+    const email = 'akhtarsameer743@gmail.com';
+    await page.goto('https://rahulshettyacademy.com/client/');
+    //----Product order using API
+    await page.waitForLoadState('networkidle');
+    await page.locator('.card-body b').first().waitFor();
+    
+    // await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/670fdb40ae2afd4c0b9d6a4f", //---Used Regex instead
+    await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*", //---URL to get the current response(orignal)
+        async route=>{
+            //Intercepting response -> APi Response -> |-Us Jumping In with fake body-| browser -> render data on frontend
+            const response = await page.request.fetch(route.request());
+            let body = JSON.stringify(fakePayloadOrder); //---Converting JS object to JSON cause cannot parse JS object
+            route.fulfill({ //----Old response new body
+                response, //--Old response 
+                body, //--New body
+            })
+
+        }
+    )
+    await page.locator('button[routerlink*="/dashboard/myorders"]').click();
+    await page.waitForResponse('https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*');
+    // await page.waitForTimeout(2000);
+    console.log(await page.locator('.mt-4').textContent());
+
+});
