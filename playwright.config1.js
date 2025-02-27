2fc9665f71c860d2fda906fdecf14c65b9e7486e
CodeNinja0615 <sameerakhtar1513@gmail.com>
2025-02-28 03:17:39 +0530

    New Config for test

---
+++ playwright.config1.js
@@ -0,0 +1,65 @@
+const { devices } = require('@playwright/test');
+const { trace } = require('console');
+const { use } = require('./playwright.config');
+
+const config = {
+  testDir: './tests',
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 7000
+  },
+  /* Run tests in files in parallel */
+  fullyParallel: false, // true | false
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html', // 'list' | 'dot' | 'line' | 'json' | 'junit' | 'html' | 'null'
+  use: {
+    launchOptions: {
+      args: ['--start-maximized'], // Start the browser maximized // '--start-fullscreen'
+    },
+    headless: false, // true | false
+    viewport: null, // Disable the default viewport // { width: 1920, height: 1080 } | null// off, on, retain-on-failure
+    screenshot: 'on', // off, on, only-on-failure
+    trace: 'on',
+    ignoreHTTPSErrors: true,
+    permissions: ['notifications', 'geolocation'],
+    // HTTP Credential
+
+  },
+  projects: [
+    {
+      name: 'Safari',
+      use: {
+        browserName: 'webkit', // 'chromium' | 'firefox' | 'webkit'
+      }
+    },
+    {
+      name: 'Mobile Safari',
+      use: {
+        browserName: 'webkit', // 'chromium' | 'firefox' | 'webkit'
+        ...devices['iPhone 11']
+      }
+    },
+    {
+      name: 'Chrome',
+      use: {
+        browserName: 'chromium', // 'chromium' | 'firefox' | 'webkit'
+      }
+    },
+    {
+      name: 'Firefox',
+      use: {
+        browserName: 'firefox', // 'chromium' | 'firefox' | 'webkit'
+      }
+    },
+    {
+      name: 'Microsoft Edge',
+      use: {
+        // ...devices['Desktop Edge'], //if used this browser will not open in full screen
+        channel: 'msedge',
+      }
+    },
+  ],
+};
+
+module.exports = config; // Export the config object
