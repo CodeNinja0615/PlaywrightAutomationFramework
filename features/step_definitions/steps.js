77797d498366b2bfccb62e39d3bfdffa51b12ff9
CodeNinja0615 <sameerakhtar1513@gmail.com>
2025-03-06 02:16:16 +0530

    Added step defintion and hooks Cucumber BDD

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,30 +1,33 @@
 const assert = require('assert')
-const { When, Then } = require('@cucumber/cucumber')
-const { Greeter } = require('../../src')
+const { expect } = require('@playwright/test');
+const { Given, When, Then } = require('@cucumber/cucumber')
 
-Given('A login to Ecommerce Application with {string} and {string}', function (string, string2) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+Given('A login to Ecommerce Application with {string} and {string}', { timeout: 10000 }, async function (username, password) {
+    this.email = username;
+    await this.poManager.getLoginPage().goTo();
+    const title = await this.poManager.getLoginPage().pageTitle();
+    await this.poManager.getLoginPage().validLogin(username, password)
 });
 
 
-When('Add {string} to Cart', function (string) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+When('Add {string} to Cart', async function (productName) {
+    await this.poManager.getDashboardPage().searchProductAddCart(productName);
+    await this.poManager.getDashboardPage().navigateToCart();
 });
 
-Then('Verify {string} is displayed in cart', function (string) {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+Then('Verify {string} is displayed in cart', async function (productName) {
+    const cartProduct = await this.poManager.getCartPage().verifyCartProduct(productName);
+    expect(productName).toEqual(cartProduct);
+    await this.poManager.getCartPage().checkOutProduct();
 });
 
 
-When('Enter valid details and place the order', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+When('Enter valid details and place the order', async function () {
+    await this.poManager.getPaymentPage().selectCountry(' India', this.email);
+    this.orderID = await this.poManager.getConfirmationPage().confirmOrder();
 });
 
-Then('Verify order is present in order history page', function () {
-    // Write code here that turns the phrase above into concrete actions
-    return 'pending';
+Then('Verify order is present in order history page', async function () {
+    await this.poManager.getConfirmationPage().navigateToOrderPage();
+    await this.poManager.getOrderPage().verifyAndViewOrder(this.orderID, this.email);
 });
---
+++ features/support/hooks.js
@@ -0,0 +1,23 @@
+const playwright = require('@playwright/test');
+const { POManager } = require('../../pageObjects/POManager'); //---Page Object Manager
+const { Before, After, AfterStep, Status } = require('@cucumber/cucumber');
+const path = require('path');
+
+
+Before(async function () {
+    const browser = await playwright.chromium.launch({ headless: false });
+    const context = await browser.newContext();
+    this.page = await context.newPage();
+    this.poManager = new POManager(this.page);
+});
+
+After(async function () {
+    console.log("Last");
+});
+
+AfterStep(async function ({ result }) {
+    // This hook will be executed after all steps, and take a screenshot on step failure
+    if (result.status === Status.FAILED) {
+        await this.page.screenshot({path: 'ss.png'});
+    }
+});
