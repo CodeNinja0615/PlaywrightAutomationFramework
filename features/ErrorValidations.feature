5ce3621b9bd688ca92c4fe802bb53a232a50f0f5
CodeNinja0615 <sameerakhtar1513@gmail.com>
2025-03-07 01:43:41 +0530

    File transfer to MAC

---
+++ features/ErrorValidations.feature
@@ -0,0 +1,5 @@
+Feature: Ecommerce validation
+
+    Scenario: Placing the order
+        Given A login to Ecommerce2 Application with "rahulshettyacadem" and "learning"
+        Then Verify Error mesaage is displayed
--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -31,3 +31,23 @@ Then('Verify order is present in order history page', async function () {
     await this.poManager.getConfirmationPage().navigateToOrderPage();
     await this.poManager.getOrderPage().verifyAndViewOrder(this.orderID, this.email);
 });
+
+Given('A login to Ecommerce2 Application with {string} and {string}', async function (userid, password) {
+    await this.page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    //get title of page and assert
+    console.log(await this.page.title());
+    await expect(this.page).toHaveTitle('LoginPage Practise | Rahul Shetty Academy');
+
+    const username = this.page.locator('#username');
+    const signIn = this.page.locator('[name="signin"]');
+
+    await username.fill(userid);
+    await this.page.locator('[id="password"]').fill(password);
+    await signIn.click();
+});
+
+Then('Verify Error mesaage is displayed', async function () {
+    const error = this.page.locator('[style*="block"]');
+    console.log(await error.textContent());
+    await expect(error).toContainText('Incorrect');
+});
