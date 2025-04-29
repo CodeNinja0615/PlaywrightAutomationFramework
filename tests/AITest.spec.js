7008365e3e296b5a0c22a6a11b69f5030ebaacdd
CodeNinja0615 <akhtarsameer743@gmail.com>
2025-04-29 19:40:30 +0530

    Ai Code added

--- tests/AITest.spec.js
+++ tests/AITest.spec.js
@@ -6,5 +6,5 @@ test('Calendar Validation', async ({ page }) => {
     await page.goto('https://rahulshettyacademy.com/seleniumPractise/#/offers');
     const text = await ai('What is the Discount price of Tomato', aiArgs)
     expect(text).toEqual("26");
-    console.log(text); 
+    console.log(text);
 });
