4995ae692effa672027d439e0ed14a23b4d18759
Sameer Akhtar <sameerakhtar@Mac.lan>
2025-04-29 19:12:16 +0530

    Adding zerostep ai automation dependency and code config added to ignore

---
+++ tests/AITest.spec.js
@@ -0,0 +1,11 @@
+const { test, expect } = require('@playwright/test');
+const { ai } = require('@zerostep/playwright');
+
+test('Calendar Validation', async ({ page }) => {
+    const aiArgs = {page, test}
+    await page.goto('https://rahulshettyacademy.com/seleniumPractise/#/offers');
+    const text = await ai('What is the Discount price of Tomato', aiArgs)
+    expect(text).toEqual("26");
+    console.log(text);
+    
+});
